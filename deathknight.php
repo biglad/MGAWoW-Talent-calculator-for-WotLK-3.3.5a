e60fdf18482872313bd9f3157f2008cbf3ad0bc9
biglad <biglad@mgawow.co.uk>
2023-03-03 19:20:39 +0000

    fix code
    fix code

--- deathknight.php
+++ deathknight.php
@@ -2,7 +2,7 @@
 <head>
 <title>CraftedRO | Death Knight Talents</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<link href="talents/deathknight/talent.css" type="text/css" rel="stylesheet">
+<link href="talents/Deathknight/talent.css" type="text/css" rel="stylesheet">
 <link rel="shortcut icon" href="images/favicon.ico">
 </head>
 <body style="background-color:#30384c">
@@ -12,16 +12,16 @@
 <a href="./index.html" title="Home"><img src="images/logo.png" alt="logo"></a>
 <br>
 <div>
-<a href="deathknight.php"><img src="../talents/class-images/class/deathknight.jpg" style="box-shadow: 0 0 5px 1px #8ab4f8;"></a>
-<a href="druid.php"><img src="../talents/class-images/class/druid.jpg"></a>
-<a href="hunter.php"><img src="../talents/class-images/class/hunter.jpg"></a>
-<a href="mage.php"><img src="../talents/class-images/class/mage.jpg"></a>
-<a href="paladin.php"><img src="../talents/class-images/class/paladin.jpg"></a>
-<a href="priest.php"><img src="../talents/class-images/class/priest.jpg"></a>
-<a href="rogue.php"><img src="../talents/class-images/class/rogue.jpg"></a>
-<a href="shaman.php"><img src="../talents/class-images/class/shaman.jpg"></a>
-<a href="warlock.php"><img src="../talents/class-images/class/warlock.jpg"></a>
-<a href="warrior.php"><img src="../talents/class-images/class/warrior.jpg"></a>
+<a href="deathknight.php"><img src="./class-images/class/deathknight.jpg" style="box-shadow: 0 0 5px 1px #8ab4f8;"></a>
+<a href="druid.php"><img src="./class-images/class/druid.jpg"></a>
+<a href="hunter.php"><img src="./class-images/class/hunter.jpg"></a>
+<a href="mage.php"><img src="./class-images/class/mage.jpg"></a>
+<a href="paladin.php"><img src="./class-images/class/paladin.jpg"></a>
+<a href="priest.php"><img src="./class-images/class/priest.jpg"></a>
+<a href="rogue.php"><img src="./class-images/class/rogue.jpg"></a>
+<a href="shaman.php"><img src="./class-images/class/shaman.jpg"></a>
+<a href="warlock.php"><img src="./class-images/class/warlock.jpg"></a>
+<a href="warrior.php"><img src="./class-images/class/warrior.jpg"></a>
 </div>
 </br>
 <div id="replaceMeWithTalents"></div>
@@ -32,13 +32,14 @@ var pageId = "deathknight";
 var variableIsSite = 1;
 var theURLtoCopy = "<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['SCRIPT_NAME']; ?>";
 </script>
-<script type="text/javascript" src="talents/deathknight/detection.js"></script>
-<script type="text/javascript" src="talents/deathknight/variables.js"></script>
-<script type="text/javascript" src="talents/deathknight/functions.js"></script>
-<script type="text/javascript" src="talents/deathknight/data.js"></script>
-<script type="text/javascript" src="talents/deathknight/donotlocalize.js"></script>
-<script type="text/javascript" src="talents/deathknight/localize.js"></script>
-<script type="text/javascript" src="talents/deathknight/arraysFill.js"></script>
-<script type="text/javascript" src="talents/deathknight/printOutTop.js"></script>
+<script type="text/javascript" src="talents/Deathknight/detection.js"></script>
+<script type="text/javascript" src="talents/Deathknight/variables.js"></script>
+<script type="text/javascript" src="talents/Deathknight/functions.js"></script>
+<script type="text/javascript" src="talents/Deathknight/data.js"></script>
+<script type="text/javascript" src="talents/Deathknight/donotlocalize.js"></script>
+<script type="text/javascript" src="talents/Deathknight/localize.js"></script>
+<script type="text/javascript" src="talents/Deathknight/arraysFill.js"></script>
+<script type="text/javascript" src="talents/Deathknight/printOutTop.js"></script>
+<BR><BR>
 </body>
 </html>
